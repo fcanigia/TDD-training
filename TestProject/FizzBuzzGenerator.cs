9a73b8a0fb56bbe875c512d06bb94505b7668829
Daniel Folgoso <daniel.folgoso@analyticalways.com>
2022-02-09 16:05:33 +0100

    first version

---
+++ TestProject/FizzBuzzGenerator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    internal class FizzBuzzGenerator
+    {
+        public static string[] GetFizzBuzzArray(int numberItems)
+        {
+            if (numberItems < 1)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            var result = new string[numberItems];
+            for (int i = 0; i < numberItems; i++)
+            {
+                var value = i + 1;
+
+                if((value % 3) == 0 && (value % 5) == 0)
+                {
+                    result[i] = "FizzBuzz";
+                }
+                else if ((value % 3) == 0)
+                {
+                    result[i] = "Fizz";
+                }
+                else if ((value % 5) == 0)
+                {
+                    result[i] = "Buzz";
+                }
+                else
+                {
+                    result[i] = value.ToString();
+                }
+            }
+
+            return result;
+        }
+    }
+}
--- TestProject/UnitTest1.cs
+++ TestProject/UnitTest1.cs
@@ -9,5 +9,45 @@ namespace TestProject
         {
             Assert.True(true);
         }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(10)]
+        public void FizzBuzzGeneratorReturnsTheArrayWithRigthLength(int numItems)
+        {
+            var result = FizzBuzzGenerator.GetFizzBuzzArray(numItems);
+
+            Assert.Equal(numItems, result.Length);
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-1)]
+        public void FizzBuzzGeneratorWithParamLessZeroThrowsException(int numItems)
+        {
+            Assert.Throws<System.ArgumentOutOfRangeException>(() => FizzBuzzGenerator.GetFizzBuzzArray(numItems));
+        }
+
+        [Theory]
+        [InlineData(2, "2")]
+        [InlineData(3, "Fizz")]
+        [InlineData(5, "Buzz")]
+        [InlineData(6, "Fizz")]
+        [InlineData(7, "7")]
+        [InlineData(15, "FizzBuzz")]
+        public void FizzBuzzGeneratorWithRightParamReturnsTheRightTheLastValue(int numItems, string lastValue)
+        {
+            var result = FizzBuzzGenerator.GetFizzBuzzArray(numItems);
+            Assert.Equal(result[numItems - 1], lastValue);
+        }
+
+        [Theory]
+        [InlineData(7, new string[] { "1", "2", "Fizz", "4", "Buzz", "Fizz", "7" })]
+        public void FizzBuzzGeneratorReturnsTheExpectedArray(int numItems, string[] expectedResult)
+        {
+            var result = FizzBuzzGenerator.GetFizzBuzzArray(numItems);
+            Assert.Equal(result, expectedResult);
+        }
+
     }
 }

e7d13db666296b522307b9ec7f0f46202ba4eef5
Facundo Canigia <facundo.canigia@analyticalways.com>
2022-02-11 08:29:07 -0300

    Gilded rose and refactor

---
+++ TestProject/GildedRose.cs
@@ -0,0 +1,247 @@
+﻿using System.Collections.Generic;
+
+namespace TestProject
+{
+    public class GildedRose
+    {
+        IList<Item> Items;
+
+        public GildedRose(IList<Item> Items)
+        {
+            this.Items = Items;
+        }
+
+        public void UpdateQuality()
+        {
+            for (var i = 0; i < Items.Count; i++)
+            {
+               if (Items[i].Name == "Aged Brie")
+                {
+                    AgedBrieProduct agedBrie = new AgedBrieProduct();
+                    Items[i] = agedBrie.UpdateQuality(Items[i]); ;
+                    break;
+                }
+
+                if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+                {
+                    BackstageTicketProduct backstagePass = new BackstageTicketProduct();
+                    Items[i] = backstagePass.UpdateQuality(Items[i]); 
+                    break;
+
+                }
+
+                if (Items[i].Name == "Sulfuras, Hand of Ragnaros")
+                {
+                    SulfurasProduct sulfuras = new SulfurasProduct();
+                    Items[i] = sulfuras.UpdateQuality(Items[i]);
+                    break;
+                }
+
+                // normal
+                NormalProduct product = new NormalProduct();
+                Items[i] = product.UpdateQuality(Items[i]);
+            }
+        }
+
+        public void UpdateQualityV2()
+        {
+            for (var i = 0; i < Items.Count; i++)
+            {
+                if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                {
+                    if (Items[i].Quality > 0)
+                    {
+                        if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                        {
+                            Items[i].Quality = Items[i].Quality - 1;
+                        }
+                    }
+                }
+                else
+                {
+                    if (Items[i].Quality < 50)
+                    {
+                        Items[i].Quality = Items[i].Quality + 1;
+
+                        if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+                        {
+                            if (Items[i].SellIn < 11)
+                            {
+                                if (Items[i].Quality < 50)
+                                {
+                                    Items[i].Quality = Items[i].Quality + 1;
+                                }
+                            }
+
+                            if (Items[i].SellIn < 6)
+                            {
+                                if (Items[i].Quality < 50)
+                                {
+                                    Items[i].Quality = Items[i].Quality + 1;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                {
+                    Items[i].SellIn = Items[i].SellIn - 1;
+                }
+
+                if (Items[i].SellIn < 0)
+                {
+                    if (Items[i].Name != "Aged Brie")
+                    {
+                        if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                        {
+                            if (Items[i].Quality > 0)
+                            {
+                                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                                {
+                                    Items[i].Quality = Items[i].Quality - 1;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            Items[i].Quality = Items[i].Quality - Items[i].Quality;
+                        }
+                    }
+                    else
+                    {
+                        if (Items[i].Quality < 50)
+                        {
+                            Items[i].Quality = Items[i].Quality + 1;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public class Item
+    {
+        public string Name { get; set; }
+        public int SellIn { get; set; }
+        public int Quality { get; set; }
+
+        public override string ToString()
+        {
+            return this.Name + ", " + this.SellIn + ", " + this.Quality;
+        }
+    }
+
+
+
+    //Bastante simple, ¿no? Bueno, ahora es donde se pone interesante:
+
+    //    - caso decremento x1
+    //    - Una vez que ha pasado la fecha recomendada de venta, la calidad se degrada al doble de velocidad
+    //    - La calidad de un artículo nunca es negativa
+    //    - El "Queso Brie envejecido" (Aged brie) incrementa su calidad a medida que se pone viejo
+    //        - Su calidad aumenta en 1 unidad cada día
+    //        - luego de la fecha de venta su calidad aumenta 2 unidades por día
+    //    - La calidad de un artículo nunca es mayor a 50
+    //    - El artículo "Sulfuras" (Sulfuras), siendo un artículo legendario, no modifica su fecha de venta ni se degrada en calidad
+    //        - Para aclarar: un artículo nunca puede tener una calidad superior a 50, sin embargo las Sulfuras siendo un artículo legendario posee una calidad inmutable de 80.
+
+
+    public interface IProduct{
+        public Item UpdateQuality(Item item);
+    }
+
+    public class NormalProduct : IProduct
+    {
+        public Item UpdateQuality(Item item)
+        {
+            int sellInUpdateValue = -1;
+            int qualityUpdateValue = -1;
+
+            if (item.SellIn <= 0 && item.Quality -2 > 0)
+            {
+                qualityUpdateValue = -2;
+            }
+
+            if (item.Quality == 0)
+            {
+                qualityUpdateValue = 0;
+            }
+
+            var updatedItem = new Item { Name = item.Name, SellIn = item.SellIn + sellInUpdateValue, Quality = item.Quality + qualityUpdateValue };
+            return updatedItem;
+        }
+    }
+
+    public class AgedBrieProduct : IProduct
+    {
+        public Item UpdateQuality(Item item)
+        {
+            int sellInUpdateValue = -1;
+            int qualityUpdateValue = 1;
+            int qualityValue;
+
+            if (item.SellIn <= 0)
+            {
+                qualityUpdateValue = 2;
+            }
+
+            if (item.Quality > 50)
+            {
+                qualityUpdateValue = 0;
+            }
+
+            qualityValue = item.Quality + qualityUpdateValue;
+
+            if (item.Quality < 50 && qualityValue > 50)
+            {
+                qualityValue = 50;
+            }
+
+            var updatedItem = new Item { Name = item.Name, SellIn = item.SellIn + sellInUpdateValue, Quality = qualityValue };
+            return updatedItem;
+        }
+    }
+
+    public class SulfurasProduct : IProduct
+    {
+        public Item UpdateQuality(Item item)
+        {
+            return item;
+        }
+    }
+
+    //    - Una "Entrada al Backstage", como el queso brie, incrementa su calidad a medida que la fecha de venta se aproxima
+    //        - si faltan 10 días o menos para el concierto, la calidad se incrementa en 2 unidades
+    //        - si faltan 5 días o menos, la calidad se incrementa en 3 unidades
+    //        - luego de la fecha de venta la calidad cae a 0
+
+    public class BackstageTicketProduct : IProduct
+    {
+        public Item UpdateQuality(Item item)
+        {
+            int sellInUpdateValue = -1;
+            int qualityUpdateValue = 1;
+
+            if (item.SellIn > 5 && item.SellIn <= 10)
+            {
+                qualityUpdateValue = 2;
+            }
+
+            if (item.SellIn >= 1 && item.SellIn <= 5)
+            {
+                qualityUpdateValue = 3;
+            }
+
+            if (item.SellIn <= 0)
+            {
+                qualityUpdateValue = -item.Quality;
+            }
+
+            var updatedItem = new Item { Name = item.Name, SellIn = item.SellIn + sellInUpdateValue, Quality = item.Quality + qualityUpdateValue };
+            return updatedItem;
+        }
+    }
+
+
+}
---
+++ TestProject/GildedRose_Should.cs
@@ -0,0 +1,201 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace TestProject
+{
+    public class GildedRose_Should
+    {
+
+        /*
+         * Descripción preliminar
+            Pero primero, vamos a introducir el sistema:
+
+            - Todos los artículos (Item) tienen una propiedad sellIn que denota el número de días que tenemos para venderlo
+            - Todos los artículos tienen una propiedad Quality que denota cúan valioso es el artículo
+            - Al final de cada día, nuestro sistema decrementa ambos valores para cada artículo mediante el método updateQuality
+
+            Bastante simple, ¿no? Bueno, ahora es donde se pone interesante:
+            
+            - caso decremento x1
+            - Una vez que ha pasado la fecha recomendada de venta, la calidad se degrada al doble de velocidad
+            - La calidad de un artículo nunca es negativa
+            - El "Queso Brie envejecido" (Aged brie) incrementa su calidad a medida que se pone viejo
+                - Su calidad aumenta en 1 unidad cada día
+                - luego de la fecha de venta su calidad aumenta 2 unidades por día
+            - La calidad de un artículo nunca es mayor a 50
+            - El artículo "Sulfuras" (Sulfuras), siendo un artículo legendario, no modifica su fecha de venta ni se degrada en calidad
+                - Para aclarar: un artículo nunca puede tener una calidad superior a 50, sin embargo las Sulfuras siendo un artículo legendario posee una calidad inmutable de 80.
+            - Una "Entrada al Backstage", como el queso brie, incrementa su calidad a medida que la fecha de venta se aproxima
+                - si faltan 10 días o menos para el concierto, la calidad se incrementa en 2 unidades
+                - si faltan 5 días o menos, la calidad se incrementa en 3 unidades
+                - luego de la fecha de venta la calidad cae a 0
+         */
+
+        [Fact]
+        public void name_not_changed()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.Equal("foo", Items[0].Name);
+        }
+
+        [Fact]
+        public void normal_product_sell_in_decrease_by_one()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "normal product", SellIn = 5, Quality = 5 }
+                                                , new Item { Name = "Aged Brie", SellIn = 5, Quality = 5 } 
+                                                , new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 10, Quality = 40 }};
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.Equal(4, Items[0].SellIn);
+        }
+
+        [Fact]
+        public void normal_product_quality_decrease_by_one()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "normal product", SellIn = 5, Quality = 5 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.Equal(4, Items[0].Quality);
+        }
+
+        [Fact]
+        public void normal_product_quality_decrease_by_double_where_expired()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "normal product", SellIn = 0, Quality = 10 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.Equal(8, Items[0].Quality);
+        }
+
+        [Fact]
+        public void normal_product_quality_never_negative()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "normal product", SellIn = 0, Quality = 1 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.Equal(0, Items[0].Quality);
+        }
+
+        [Fact]
+        public void aged_brie_quality_should_increase_by_one()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 5, Quality = 5 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.Equal(6, Items[0].Quality);
+        }
+
+        [Fact]
+        public void aged_brie_quality_should_increase_by_two_when_expired()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 0, Quality = 5 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.Equal(7, Items[0].Quality);
+        }
+
+        [Fact]
+        public void aged_brie_quality_can_not_be_greater_than_fifty()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 0, Quality = 49 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.Equal(50, Items[0].Quality);
+        }
+
+        [Fact]
+        public void aged_brie_quality_doesnt_change_if_greater_than_fifty()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 0, Quality = 55 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.Equal(55, Items[0].Quality);
+        }
+
+        [Fact]
+        public void sulfuras_static_sell_in()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 10, Quality = 40 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.Equal(10, Items[0].SellIn);
+        }
+
+        [Fact]
+        public void sulfuras_static_quality()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 10, Quality = 81 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.Equal(81, Items[0].Quality);
+        }
+
+        [Fact]
+        public void backstage_increase_quality_by_one_when_sell_in_greater_than_ten()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 11, Quality = 40 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.Equal(41, Items[0].Quality);
+        }
+
+        [Fact]
+        public void backstage_increase_quality_by_two_when_sell_in_between_six_and_ten()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 10, Quality = 40 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.Equal(42, Items[0].Quality);
+        }
+
+        [Fact]
+        public void backstage_increase_quality_by_three_when_sell_in_between_one_and_five()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 4, Quality = 40 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.Equal(43, Items[0].Quality);
+        }
+
+        [Fact]
+        public void backstage_quality_to_zero_when_expired()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = -1, Quality = 40 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.Equal(0, Items[0].Quality);
+        }
+
+        [Fact]
+        public void normal_product_with_sulfuras_name_sell_in_decrease_by_one()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "normal Sulfuras product", SellIn = 10, Quality = 40 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.Equal(9, Items[0].SellIn);
+        }
+
+        [Fact]
+        public void normal_product_with_sulfuras_name_quality_decrease_by_one()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "normal Sulfuras product", SellIn = 10, Quality = 40 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.Equal(39, Items[0].Quality);
+        }
+
+        [Fact]
+        public void normal_product_with_minus_ten_sell_in_decrease_by_one()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "normal Sulfuras product", SellIn = -10, Quality = 40 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.Equal(-11, Items[0].SellIn);
+        }
+
+    }
+}
+
+
